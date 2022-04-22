ea49ee7cf026ffd37e05a395465b7a0098927aea
StarlightC <mail.starlightc@gmail.com>
2022-04-22 14:13:48 +0800

    create android project

---
+++ sample/src/main/kotlin/com/starlightc/videoview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.starlightc.videoview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

b4cd29cfc014cc19b8c1c5705070e7759b34295b
StarlightC <mail.starlightc@gmail.com>
2022-07-11 19:00:39 +0800

    adjust loading

--- hbvideoview/src/main/kotlin/com/starlightc/videoview/processing/DefaultInfoActor.kt
+++ hbvideoview/src/main/kotlin/com/starlightc/videoview/processing/DefaultInfoActor.kt
@@ -22,14 +22,14 @@ class DefaultInfoActor:  InfoActor{
                 view.retryCount = 0
             }
             2 -> { //开始缓冲
-                view.videoUI?.showLoading()
-                //danmakuView?.pause()
+                if (view.playerState != PlayerState.CACHING) {
+                    view.videoUI?.showLoading()
+                    //danmakuView?.pause()
+                }
             }
             3 -> { //从缓冲结束
-                if (view.playerState != PlayerState.CACHING) {
-                    view.videoUI?.hideLoading()
-                    view.retryCount = 0
-                }
+                view.videoUI?.hideLoading()
+                view.retryCount = 0
             }
             4 -> { //视频方向更改
                 view.setVideoRotation(extra.toFloat())
